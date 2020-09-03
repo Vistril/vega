38abb13bc428258081d3456c7c8086997dee0df4
Vistril <scamomilegm@gmail.com>
2020-09-02 23:27:43 -0600

    trello and tracking

---
+++ VegaCore/Tasks/CheckBots.js
@@ -0,0 +1,27 @@
+const { Command } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
+class CheckBots extends Command {
+    constructor() {
+        super('bots status', {
+            aliases: ['bots status']
+        });
+        this.NDMBRunning = false;
+        this.TranslatoRunning = false;
+        this.VEGARunning = false;
+    }
+
+    async exec({ channel, guild, author }) {
+        this.channels.cache.get(750939678328225842).send("n!info");
+        if (author.id == "251990622339203072") this.NDMBRunning = true;
+        let embed = new MessageEmbed()
+            .setTitle("BitKit Bots Roundup")
+            .addFields(
+                {
+                    "name": "NDMB", value: this.NDMBRunning ? "Online!" : "Offline!",
+                }
+            )
+        channel.send(embed);
+    }
+}
+
+module.exports = CheckBots;
