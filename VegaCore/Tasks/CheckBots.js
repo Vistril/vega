0c1e85db6f2f1f0b0b4c82403f4fce9acd9e9f04
Vistril <scamomilegm@gmail.com>
2020-09-02 23:35:19 -0600

    coming soon

--- VegaCore/Tasks/CheckBots.js
+++ VegaCore/Tasks/CheckBots.js
@@ -2,8 +2,8 @@ const { Command } = require('discord-akairo');
 const { MessageEmbed } = require('discord.js');
 class CheckBots extends Command {
     constructor() {
-        super('bots status', {
-            aliases: ['bots status']
+        super('checkbots', {
+            aliases: ['checkbots']
         });
         this.NDMBRunning = false;
         this.TranslatoRunning = false;
@@ -11,7 +11,7 @@ class CheckBots extends Command {
     }
 
     async exec({ channel, guild, author }) {
-        this.channels.cache.get(750939678328225842).send("n!info");
+        this.client.channels.cache.get("750939678328225842").send("n!info");
         if (author.id == "251990622339203072") this.NDMBRunning = true;
         let embed = new MessageEmbed()
             .setTitle("BitKit Bots Roundup")
